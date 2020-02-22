f1dfbf8f0fa5b4e128378cc1957b7d02f1cb10ab
Yanqing Li <li1453647259@gmail.com>
2020-02-22 17:36:53 -0500

    Optimization of moving system, track (bone), offset between aiming ring and gun aiming position
    Now the tank can go forward backward combining with turning the hull. The camera will now follow focusing at the turrent of the tank. The offset between the track's bone and wach wheel was now right and I adjusted every single of them.

---
+++ tank/Assets/Scripts/TankCamera.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankCamera : MonoBehaviour
+{
+    private const float YLimit_Min = -50.0f;
+    private const float YLimit_Max = 89.0f;
+
+    public Transform LookAt;
+    public Transform camTransform;
+
+    private Camera cam;
+
+    private float distance = 10.0f;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    private float sensivityX = 4.0f;
+    private float sensivityY = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        camTransform = transform;
+        cam = Camera.main;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        camTransform.position = LookAt.position + rotation * dir;
+        camTransform.LookAt(LookAt.position);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            distance += 1;
+        }
+        //Zoom in  
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            distance -= 1;
+        }
+
+        currentX += Input.GetAxis("Mouse X");
+        currentY -= Input.GetAxis("Mouse Y");
+
+        currentY = Mathf.Clamp(currentY, YLimit_Min, YLimit_Max);
+    }
+}
