ac58cb8110ea4d77ac84a7813a2c97b2944a6b5a
Yanqing Li <li1453647259@gmail.com>
2020-02-26 01:30:58 -0500

    Revert "adjusted aiming again"
    This reverts commit 632da7608c74ad553afa05b64b6476464625a418.

--- tank/Assets/Scripts/TurrentTester.cs
+++ tank/Assets/Scripts/TurrentTester.cs
@@ -5,7 +5,6 @@ namespace TurretDemo
 {
     public class TurrentTester : MonoBehaviour
     {
-        Camera cam;
         public TurrentRotation[] turret;
         public Vector3 targetPos;
         public Transform targetTransform;
@@ -13,39 +12,15 @@ namespace TurretDemo
         [Space]
         public bool turretsIdle = false;
 
-        private void Start()
-        {
-            cam = GetComponent<Camera>();
-        }
         private void Update()
         {
             // Toggle turret idle.
             if (Input.GetKeyDown(KeyCode.E))
                 turretsIdle = !turretsIdle;
 
-            Ray ray = cam.ScreenPointToRay(new Vector3(960, 583, 0));
-            RaycastHit hit;
-            if(Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity))
-            {
-                Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.yellow);
-                targetPos = hit.point;
-                Debug.Log("Did Hit");
-            }
-            else
-            {
-                Debug.DrawRay(ray.origin, ray.direction * 1000, Color.white);
-                targetPos = ray.direction * 1000;
-                Debug.Log("Did not Hit");
-            }
-
-
             // When a transform is assigned, pass that to the turret. If not,
             // just pass in whatever this is looking at.
-            //targetPos = transform.TransformPoint(Vector3.forward * 200.0f);
-
-
-
-            //print(ray.direction);
+            targetPos = transform.TransformPoint(Vector3.forward * 200.0f);
             foreach (TurrentRotation tur in turret)
             {
                 if (targetTransform == null)
