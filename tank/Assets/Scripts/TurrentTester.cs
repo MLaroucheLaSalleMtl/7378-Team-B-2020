653652370938d840ce3918243d82568b4c84364e
Yanqing Li <li1453647259@gmail.com>
2020-02-26 17:46:53 -0500

    Adjusted the aiming system
    1. The aiming on the previous version was based on the Free Respect resolution, which can cause the fixed aiming offset between the reticle and where the gun is pointing at to be mistaken on different resolution. I've recalculate the offset scale of the reticle on the canvas and use it for the gun to aim. If the reticle is pointing at a place further than 1000 units, the gun will just aim at the 1000 unit point, otherwise the gun will rotate to the point wherever the reticle lays on.
    2. I've changed the color space from Gamma to Linear, in case of a better rendering.

--- tank/Assets/Scripts/TurrentTester.cs
+++ tank/Assets/Scripts/TurrentTester.cs
@@ -12,6 +12,14 @@ namespace TurretDemo
         [Space]
         public bool turretsIdle = false;
 
+        Camera cam;
+
+        void Start()
+        {
+            cam = GetComponent<Camera>();
+        }
+
+
         private void Update()
         {
             // Toggle turret idle.
@@ -19,8 +27,24 @@ namespace TurretDemo
                 turretsIdle = !turretsIdle;
 
             // When a transform is assigned, pass that to the turret. If not,
-            // just pass in whatever this is looking at.
-            targetPos = transform.TransformPoint(Vector3.forward * 200.0f);
+            // just pass in whatever the reticle is looking at.
+
+            Ray ray = cam.ScreenPointToRay(new Vector3(960, 583, 0));
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray.origin, ray.direction * 1000, out hit, Mathf.Infinity))
+            {
+                Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.yellow);
+                targetPos = hit.point;
+                Debug.Log("Did Hit");
+            }
+            else
+            {
+                Debug.DrawRay(ray.origin, ray.direction * 1000, Color.white);
+                targetPos = ray.direction * 1000;
+                Debug.Log("Did not Hit");
+            }
+
             foreach (TurrentRotation tur in turret)
             {
                 if (targetTransform == null)
---
+++ tank/Assets/TankUI/Scripts/Weapon/BulletDamage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDamage : MonoBehaviour
+{
+    public GameObject shellExplosionPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void onTriggerEvent(Collider collider) {
+        GameObject.Instantiate(shellExplosionPrefab, transform.position, transform.rotation);
+    }
+}
