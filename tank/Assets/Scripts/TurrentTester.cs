212a94bec1e326bf51efc0b9db13afdeb5b89d2a
Yanqing Li <li1453647259@gmail.com>
2020-02-26 18:20:37 -0500

    123

--- tank/Assets/Scripts/TurrentTester.cs
+++ tank/Assets/Scripts/TurrentTester.cs
@@ -12,14 +12,6 @@ namespace TurretDemo
         [Space]
         public bool turretsIdle = false;
 
-        Camera cam;
-
-        void Start()
-        {
-            cam = GetComponent<Camera>();
-        }
-
-
         private void Update()
         {
             // Toggle turret idle.
@@ -27,24 +19,8 @@ namespace TurretDemo
                 turretsIdle = !turretsIdle;
 
             // When a transform is assigned, pass that to the turret. If not,
-            // just pass in whatever the reticle is looking at.
-
-            Ray ray = cam.ScreenPointToRay(new Vector3(960, 583, 0));
-
-            RaycastHit hit;
-            if (Physics.Raycast(ray.origin, ray.direction * 1000, out hit, Mathf.Infinity))
-            {
-                Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.yellow);
-                targetPos = hit.point;
-                Debug.Log("Did Hit");
-            }
-            else
-            {
-                Debug.DrawRay(ray.origin, ray.direction * 1000, Color.white);
-                targetPos = ray.direction * 1000;
-                Debug.Log("Did not Hit");
-            }
-
+            // just pass in whatever this is looking at.
+            targetPos = transform.TransformPoint(Vector3.forward * 200.0f);
             foreach (TurrentRotation tur in turret)
             {
                 if (targetTransform == null)
