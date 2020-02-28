1fc36202fb925c76fb1e128c36587ab942762c19
2216732050 <46677738+2216732050@users.noreply.github.com>
2020-02-28 16:06:19 -0500

    update my project

--- tank/Assets/Scripts/TurrentTester.cs
+++ tank/Assets/Scripts/TurrentTester.cs
@@ -5,23 +5,39 @@ namespace TurretDemo
 {
     public class TurrentTester : MonoBehaviour
     {
-        public TurrentRotation[] turret;
+        public TurretRotation[] turret;
         public Vector3 targetPos;
         public Transform targetTransform;
-
+        Camera cam;
         [Space]
         public bool turretsIdle = false;
 
+        void Start()
+        {
+            cam = GetComponent<Camera>();
+        }
         private void Update()
         {
-            // Toggle turret idle.
             if (Input.GetKeyDown(KeyCode.E))
                 turretsIdle = !turretsIdle;
 
-            // When a transform is assigned, pass that to the turret. If not,
-            // just pass in whatever this is looking at.
-            targetPos = transform.TransformPoint(Vector3.forward * 200.0f);
-            foreach (TurrentRotation tur in turret)
+            Ray ray = cam.ScreenPointToRay(new Vector3(960, 583, 0));
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray.origin, ray.direction * 10, out hit, Mathf.Infinity))
+            {
+                Debug.DrawRay(ray.origin, ray.direction * 1000, Color.yellow);
+                targetPos = hit.point;
+                //Debug.Log("Did Hit");
+            }
+            else
+            {
+                Debug.DrawRay(ray.origin, ray.direction * 1000, Color.white);
+                targetPos = ray.direction * 1000;
+                //Debug.Log("did not hit");
+            }
+
+            foreach (TurretRotation tur in turret)
             {
                 if (targetTransform == null)
                     tur.SetAimpoint(targetPos);
