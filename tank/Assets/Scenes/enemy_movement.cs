3ed833159de2997effae40383b20884712c477e5
2216732050 <46677738+2216732050@users.noreply.github.com>
2020-02-28 22:46:02 -0500

    merged project

--- tank/Assets/Scenes/enemy_movement.cs
+++ tank/Assets/Scenes/enemy_movement.cs
@@ -1,18 +1,99 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class enemy_movement : MonoBehaviour
 {
+    public Transform player;
+    NavMeshAgent enemy;
+    Vector3 destination;
+    public float distance=50;//the enemy will get close to the player and stop "distance" away from player
+    RaycastHit hit;
+    RaycastHit linehit;
+    public LayerMask LayerMask;
+    public LayerMask layerMask;
+    public bool ReachDestination = false;
+    float timer = 0f;
+    public float waittime = 3f;
+    public bool collider_exist = false;
+    public float DistanceBetweenTwo;
+
+    public bool isdetected = false;
+    public float detect_distance = 50;
     // Start is called before the first frame update
     void Start()
     {
+        enemy = this.GetComponent<NavMeshAgent>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        DistanceBetweenTwo= Vector3.Distance(transform.position, player.position);
         
+
+        if(DistanceBetweenTwo<=detect_distance)
+        {
+            isdetected = true;
+        }
+
+
+
+
+
+
+        if((ReachDestination==false||collider_exist==true)&&isdetected==true)
+        {
+
+            timer += Time.deltaTime;
+            if (timer > waittime)
+            {
+                destination = player.transform.position;
+                SetDestination();
+            }
+
+        }
+        else if(ReachDestination==true&&collider_exist==false)
+        {
+
+            destination = transform.position;
+            SetDestination();
+        }
+        
+        if (DistanceBetweenTwo<=distance)
+        {
+            
+                timer = 0f;
+                
+                ReachDestination = true;
+            
+            
+        }
+        else
+        {
+            ReachDestination = false;
+        }
+
+
+        if(Physics.Linecast(transform.position,player.position,out linehit,LayerMask))
+        {
+            collider_exist = true;
+        }
+        else
+        {
+            collider_exist = false;
+        }
+        
+
+    }
+    public void SetDestination()
+    {
+        
+        enemy.SetDestination(destination);
+    }
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(3f);
     }
 }
--- tank/Assets/Scripts/TurrentTester.cs
+++ tank/Assets/Scripts/TurrentTester.cs
@@ -5,39 +5,23 @@ namespace TurretDemo
 {
     public class TurrentTester : MonoBehaviour
     {
-        public TurretRotation[] turret;
+        public TurrentRotation[] turret;
         public Vector3 targetPos;
         public Transform targetTransform;
-        Camera cam;
+
         [Space]
         public bool turretsIdle = false;
 
-        void Start()
-        {
-            cam = GetComponent<Camera>();
-        }
         private void Update()
         {
+            // Toggle turret idle.
             if (Input.GetKeyDown(KeyCode.E))
                 turretsIdle = !turretsIdle;
 
-            Ray ray = cam.ScreenPointToRay(new Vector3(960, 583, 0));
-
-            RaycastHit hit;
-            if (Physics.Raycast(ray.origin, ray.direction * 10, out hit, Mathf.Infinity))
-            {
-                Debug.DrawRay(ray.origin, ray.direction * 1000, Color.yellow);
-                targetPos = hit.point;
-                //Debug.Log("Did Hit");
-            }
-            else
-            {
-                Debug.DrawRay(ray.origin, ray.direction * 1000, Color.white);
-                targetPos = ray.direction * 1000;
-                //Debug.Log("did not hit");
-            }
-
-            foreach (TurretRotation tur in turret)
+            // When a transform is assigned, pass that to the turret. If not,
+            // just pass in whatever this is looking at.
+            targetPos = transform.TransformPoint(Vector3.forward * 200.0f);
+            foreach (TurrentRotation tur in turret)
             {
                 if (targetTransform == null)
                     tur.SetAimpoint(targetPos);
