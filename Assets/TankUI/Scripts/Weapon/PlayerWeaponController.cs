70330b07b24181b3483421f9d0919b295b9426ff
Yanqing Li <li1453647259@gmail.com>
2020-03-03 00:56:30 -0500

    Revert "Revert "Created Loading Scene, optimized GameMenu Scene""
    This reverts commit 1417495b4a7eb44c356f1df4a1555bfb34b7846a.

--- Assets/TankUI/Scripts/Weapon/PlayerWeaponController.cs
+++ Assets/TankUI/Scripts/Weapon/PlayerWeaponController.cs
@@ -7,6 +7,7 @@ public class PlayerWeaponController : MonoBehaviour
     public GameObject projectile;
     public GameObject playerShootPosition;
     public float shellSpeed = 250;
+    public static bool fire = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,10 +18,11 @@ public class PlayerWeaponController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButtonDown(0) || fire) {
             this.gameObject.GetComponent<AudioSource>().Play();
             GameObject shell = GameObject.Instantiate(projectile, playerShootPosition.transform.position, playerShootPosition.transform.rotation);
             shell.GetComponent<Rigidbody>().velocity = shell.transform.forward * shellSpeed;
+            fire = false;
         }
 	}
 }
