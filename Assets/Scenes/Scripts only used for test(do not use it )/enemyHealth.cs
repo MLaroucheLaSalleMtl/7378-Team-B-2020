c717ea59bc6306f8978752f57f82dfc04d437cf4
2216732050 <46677738+2216732050@users.noreply.github.com>
2020-03-08 20:22:50 -0400

    update to see if it can be pushed

---
+++ Assets/Scenes/Scripts only used for test(do not use it )/enemyHealth.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyHealth : MonoBehaviour
+{
+    public float maxhealth = 100f;
+    public float CurrentHealth;
+   
+    public GameObject shell;
+    public GameObject enemy;
+    public float damage = 50f;
+
+    public bool DamageOnce;
+    public EndCondition EndCondition;
+
+
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentHealth = maxhealth;
+        
+        
+    }
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void DoDamage()
+    {
+        if (DamageOnce)
+        {
+            CurrentHealth = CurrentHealth - damage;
+            DamageOnce = false;
+            
+        }
+
+
+    }
+}
---
+++ Assets/Scenes/Scripts only used for test(do not use it )/explosion.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosion : MonoBehaviour
+{
+    public GameObject explode;
+    public GameObject self;
+    public float speed = 10;
+    public float timer = 0;
+    public float waittime = 1;
+    public bool collided = false;
+    public bool onlyonce=false;
+    RaycastHit hit;
+    public float ExplodeRadius = 10;
+    public LayerMask LayerMask;
+    public float ExplodeDistance;//the distance between shell and enemy, it is used to figure out how much damage the enemy will take
+    public EnemyHealth EnemyHealth;
+    void Start()
+    {
+        
+    }
+
+
+    private void Update()
+    {
+        if(collided)
+        {
+            if(onlyonce==false)
+            {
+                timer += Time.deltaTime;
+                if (timer >= waittime)
+                {
+
+                    self.SetActive(false);
+                }
+                if (timer > waittime)
+                {
+
+                    onlyonce = true;
+                }
+            }
+           
+        }
+        
+    }
+    // Update is called once per frame
+
+
+   
+    private void OnCollisionEnter(Collision collision)
+    {
+        collided = true;
+        Instantiate(explode, self.transform.position,Quaternion.identity);
+        if (Physics.SphereCast(self.transform.position, ExplodeRadius, self.transform.forward, out hit, 0, LayerMask))
+        {
+            if( hit.transform.tag=="Enemy")
+            {
+                ExplodeDistance = Vector3.Distance(self.transform.position, hit.transform.position);
+                if(ExplodeDistance<3)
+                {
+                    EnemyHealth.DoDamage();
+                }
+            }
+        }
+
+        
+    }
+
+}
+
+
