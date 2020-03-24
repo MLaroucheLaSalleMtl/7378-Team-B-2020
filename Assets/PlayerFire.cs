e245488e8aeef7d79aff2b9de614c1d1ea7f2cc5
2216732050 <46677738+2216732050@users.noreply.github.com>
2020-03-24 15:28:03 -0400

    new fire and enemy movement

---
+++ Assets/PlayerFire.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFire : MonoBehaviour
+{
+    public GameObject AP;
+    public GameObject HE;
+    public Transform position;
+    public float speed = 250f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject shell = Instantiate(AP, position.position, position.transform.rotation);
+            shell.GetComponent<Rigidbody>().velocity = shell.transform.forward * speed;
+        }
+    }
+}
