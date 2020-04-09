f3bda4f81eedda4bb11ef30ccbf2250d6e4785fe
yourforeverbear <60150483+yourforeverbear@users.noreply.github.com>
2020-04-09 12:00:12 -0400

    add nextlevelprogress prefab,fixed bug of player health text, add posion sphere ctrl

---
+++ Assets/TankUI/For LevelFinal/Scripts/PosionSphereCtrl.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PosionSphereCtrl : MonoBehaviour
+{
+    public int endTime = 300;
+    public float reduceDelta = 0f;
+    public Vector3 reduceDeltaVec=Vector3.zero;
+    public string playerTag = "Player";
+    public string playerLayer = "TankBody";
+    public bool isPlayerExist = false;
+    public PlayerHealth playerHealth = null;
+    public float playerHpDeduceVal = 2f;
+
+    private void Awake()
+    {
+        playerHealth = GameObject.FindWithTag(playerTag).GetComponent<PlayerHealth>();
+    }
+
+    public void Start()
+    {
+        reduceDelta = transform.localScale.x / endTime;
+        reduceDeltaVec=new Vector3(reduceDelta,reduceDelta,reduceDelta);
+        
+    }
+
+    private void Update()
+    {
+        transform.localScale-=reduceDeltaVec*Time.deltaTime;
+        if (!isPlayerExist)
+            ReducePlayerHp();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals(playerTag, StringComparison.CurrentCultureIgnoreCase))
+        {
+            isPlayerExist = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Equals(playerTag, StringComparison.CurrentCultureIgnoreCase))
+        {
+            isPlayerExist = false;
+        }
+    }
+
+    private void ReducePlayerHp()
+    {
+        playerHealth.DoDamage(playerHpDeduceVal*Time.deltaTime);
+    }
+}
---
+++ Assets/TankUI/For LevelFinal/Scripts/WinProgressObjCtrl.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinProgressObjCtrl : MonoBehaviour
+{
+    public float rotSpeed = 2f;
+    public float curProgress = 0f;
+    //each secs slider add val
+    public float progressAddDeltaVal = 0.1f;
+    public Action onProgressEndCallBack;
+    public bool isStartCalcWinProgress = false;
+    public string playerTag = "Player";
+    public GameObject sliderCanvas = null;
+    public Slider slider;
+    void Update()
+    {
+        transform.Rotate(Vector3.up*Time.deltaTime*rotSpeed);
+        if (isStartCalcWinProgress)
+        {
+            if (sliderCanvas != null && !sliderCanvas.gameObject.activeInHierarchy)
+            {
+                sliderCanvas.gameObject.SetActive(true);
+            }
+            curProgress += progressAddDeltaVal * Time.deltaTime;
+            if (slider != null) slider.value = curProgress;
+            if (slider.value >= 0.99f)
+            {
+                if (onProgressEndCallBack != null)
+                {
+                    onProgressEndCallBack();
+                }
+
+            }
+        }
+    }
+
+    public void RegisterEndEvt(Action endEvt)
+    {
+        if (endEvt != null)
+        {
+            onProgressEndCallBack = endEvt;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals(playerTag, StringComparison.CurrentCultureIgnoreCase))
+        {
+            isStartCalcWinProgress = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Equals(playerTag, StringComparison.CurrentCultureIgnoreCase))
+        {
+            isStartCalcWinProgress = false;
+            sliderCanvas.SetActive(false);
+        }
+    }
+
+  
+}
