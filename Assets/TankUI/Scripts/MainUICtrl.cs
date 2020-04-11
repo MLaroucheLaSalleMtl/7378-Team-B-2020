e1198c5c6d362ea1443730ef1acbca1cccb77b7f
yourforeverbear <60150483+yourforeverbear@users.noreply.github.com>
2020-04-11 01:14:05 -0400

    add boot scene before menu scene, In order to solve the game menu bug, the tankcamera script was fixed

---
+++ Assets/TankUI/Scripts/MainUICtrl.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainUICtrl : MonoBehaviour
+{
+    public GameObject OptionWnd;
+
+    public void ShowWnd(bool flag)
+    {
+        if (flag)
+        {
+            Time.timeScale = 0f;
+            OptionWnd.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            OptionWnd.SetActive(false);
+        }
+    }
+
+    public void BackToMenu()
+    {
+        ShowWnd(false);
+        SceneManager.LoadScene(2);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (TankCamera.Ins != null)
+            {
+                if (OptionWnd.gameObject.activeInHierarchy)
+                    TankCamera.Ins.LockCursor();
+                else
+                    TankCamera.Ins.UnLockCursor();
+            }
+
+            ShowWnd(!OptionWnd.gameObject.activeInHierarchy);
+        }
+    }
+}
