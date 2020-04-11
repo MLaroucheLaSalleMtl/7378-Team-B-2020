12c17b285dc35eaed435cb36e19c825458c20daf
yourforeverbear <60150483+yourforeverbear@users.noreply.github.com>
2020-04-11 00:39:23 -0400

    modify game menu login panel,add player name ui canvas

---
+++ Assets/TankUI/Scripts/ModuleRoot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModuleRoot : MonoBehaviour
+{
+    public static ModuleRoot Ins;
+
+    private void Awake()
+    {
+        Ins = this;
+    }
+
+    public ModuleData ModuleData;
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
